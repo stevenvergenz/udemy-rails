cb981a7b40d965385be3435b8241634a48a2a755
Steven Vergenz <vergenzs@gmail.com>
2018-02-21 17:20:13 -0800

    Set up database schema, first model!

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,28 @@
+class PostsController < ApplicationController
+  
+#  CRUD = create, read, update, delete
+
+  # view
+  def index
+  end
+
+  # view
+  def show
+  end
+
+  # view
+  def new
+  end
+
+  # used by new
+  def create
+  end
+
+  # view
+  def edit
+  end
+
+  def update; end
+  def delete; end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #get 'posts', to: 'posts#index'
+  #get 'show', to: 'posts#show'
+  resources :posts
+
+  # set an index page (not a redirect)
+  root to: "posts#index"
+
 end
